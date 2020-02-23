770de35f3573275faf075aa98631f68cee0c726d
shivamsoni021 <60838323+shivamsoni021@users.noreply.github.com>
2020-02-23 20:11:27 +0530

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,234 @@
+import java.awt.*;
+import java.awt.event.*;
+public class Calculator implements ActionListener,KeyListener{
+    String s= "";
+    Calculator()
+    {
+    Frame f= new Frame("Calculator");
+    TextField t1=new TextField("");
+    Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,bp,bs,bm,bd,br,bc,bdc,bpl,bpr;
+    
+    b0= new Button("0");
+    b1= new Button("1");
+    b2= new Button("2");
+    b3= new Button("3");
+    b4= new Button("4");
+    b5= new Button("5");
+    b6= new Button("6");
+    b7= new Button("7");
+    b8= new Button("8");
+    b9= new Button("9");
+    bp= new Button("+");
+    bs= new Button("-");
+    bm= new Button("x");
+    bd= new Button("/");
+    br= new Button("=");
+    bc= new Button("CE");
+    bdc= new Button(".");
+    bpl= new Button("(");
+    bpr= new Button(")");
+    
+    t1.setBounds(30, 50 , 210, 30);
+    
+    b1.setBounds(30,100,45,35);
+    b2.setBounds(85,100,45,35);
+    b3.setBounds(140,100,45,35);
+    bp.setBounds(195,100,45,35);
+    b4.setBounds(30,145,45,35);
+    b5.setBounds(85,145,45,35);
+    b6.setBounds(140,145,45,35);
+    bs.setBounds(195,145,45,35);
+    b7.setBounds(30,190,45,35);
+    b8.setBounds(85,190,45,35);
+    b9.setBounds(140,190,45,35);    
+    bm.setBounds(195,190,45,35);
+    b0.setBounds(30,235,100,35);
+    bdc.setBounds(140,235,45,35);
+    bd.setBounds(195,235,45,35);
+    br.setBounds(195,280,45,35);
+    bc.setBounds(140,280,45,35);
+    bpl.setBounds(30,280,45,35);
+    bpr.setBounds(85,280,45,35);
+    
+    b0.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e)
+    {
+    s=s+"0";
+    t1.setText(s);
+    }
+    });
+    b1.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+"1";
+    t1.setText(s);
+    }
+    });
+    b2.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+"2";
+    t1.setText(s);
+    }
+    });
+    b3.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+"3";
+    t1.setText(s);
+    }
+    });
+    b4.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+"4";
+    t1.setText(s);
+    }
+    });
+    b5.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+"5";
+    t1.setText(s);
+    }
+    });
+    b6.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+"6";
+    t1.setText(s);;
+    }
+    });
+    b7.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+"7";
+    t1.setText(s);
+    }
+    });
+    b8.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+"8";
+    t1.setText(s);
+    }
+    });
+    b9.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+"9";
+    t1.setText(s);
+    }
+    });
+    bp.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+" + ";
+    t1.setText(s);
+    }
+    });
+    bs.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+" - ";
+    t1.setText(s);
+    }
+    });
+    bm.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+" * ";
+    t1.setText(s);
+    }
+    });
+    bd.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+" / ";
+    t1.setText(s);
+    }
+    });
+    bdc.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=s+".";
+    t1.setText(s);
+    }
+    });
+    
+    br.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s=t1.getText();
+    t1.setText(""+EvaluateString.evaluate(s.trim()));
+    }});
+   
+    bc.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    s="";
+    t1.setText(s);
+    }
+    });
+    
+    t1.addKeyListener(new KeyAdapter() {
+         public void keyPressed(KeyEvent e) {
+             
+             if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+                 s=t1.getText();
+                 t1.setText(""+EvaluateString.evaluate(s.trim()));
+            }
+         }
+      });
+    
+    f.addKeyListener(this);
+    /*
+    b.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent e){
+    t1.setText("");
+    }
+    });
+    */
+    
+    f.add(t1);
+    f.add(b0);
+    f.add(b1);
+    f.add(b2);
+    f.add(b3);
+    f.add(b4);
+    f.add(b5);
+    f.add(b6);
+    f.add(b7);
+    f.add(b8);
+    f.add(b9);
+    f.add(bp);
+    f.add(bs);
+    f.add(bm);
+    f.add(bd);
+    f.add(br);
+    f.add(bc);
+    f.add(bdc);
+    f.add(bpl);
+    f.add(bpr);
+    
+    f.setLayout(null);
+    f.setSize(270,400); 
+    f.setVisible(true);
+    f.setBackground(Color.darkGray);
+    f.addWindowListener(new WindowAdapter(){
+    public void windowClosing(WindowEvent we){
+    System.exit(0);
+    }
+    });
+}
+
+    public static void main(String[] args){
+    Calculator c= new Calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyTyped(KeyEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
